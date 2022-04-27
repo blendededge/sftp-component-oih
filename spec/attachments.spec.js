b8e5ca705675ea7ec9382dbf645e10b9765a2cc1
Stephen Hyde <shh2112@gmail.com>
2022-04-27 11:45:09 -0400

    update AttachmentProcessor used in attachments test

--- spec/attachments.spec.js
+++ spec/attachments.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const Stream = require('stream');
-const { AttachmentProcessor } = require('../lib/attachmentProcessor');
+const { AttachmentProcessor } = require('@blendededge/ferryman-extensions');
 const attachments = require('../lib/attachments');
 
 // stub things
